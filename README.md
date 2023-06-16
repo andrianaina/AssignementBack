4545fe165dce952f21fe33830e2ed19870e163e9
francky13 <60479602+francky13@users.noreply.github.com>
2023-06-16 22:04:12 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# AssignementBack
+# MBDSMagascar2022_2023_api
--- server.js
+++ server.js
@@ -1,14 +1,62 @@
-//Install express server
-const express = require("express");
-const path = require("path");
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let assignment = require('./routes/assignments');
 
-const app = express();
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//mongoose.set('debug', true);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/assignment-app/"));
+// remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
+const uri = 'mongodb+srv://mb:toto@cluster0.5e6cs7n.mongodb.net/assignments?retryWrites=true&w=majority';
 
-app.get("/*", function (req, res) {
- res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify:false
+};
+
+mongoose.connect(uri, options)
+  .then(() => {
+    console.log("Connecté à la base MongoDB assignments dans le cloud !");
+    console.log("at URI = " + uri);
+    console.log("vérifiez with http://localhost:8010/api/assignments que cela fonctionne")
+    },
+    err => {
+      console.log('Erreur de connexion: ', err);
+    });
+
+// Pour accepter les connexions cross-domain (CORS)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+
+// Pour les formulaires
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 8010;
+
+// les routes
+const prefix = '/api';
+
+app.route(prefix + '/assignments')
+  .get(assignment.getAssignments)
+  .post(assignment.postAssignment)
+  .put(assignment.updateAssignment);
+
+app.route(prefix + '/assignments/:id')
+  .get(assignment.getAssignment)
+  .delete(assignment.deleteAssignment);
+  
+
+// On démarre le serveur
+app.listen(port, "0.0.0.0");
+console.log('Serveur démarré sur http://localhost:' + port);
+
+module.exports = app;
+
+
